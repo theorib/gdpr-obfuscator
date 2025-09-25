0d22804d9c85e480eb1f299409786189a1ed3832
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-09-25 09:57:17 -0300

    🏗️ build(infrastructure): provision S3 bucket and upload test data - create S3 bucket with project and environment tags - upload complex PII test data file - export bucket name and data object key

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -1,10 +1,33 @@
 """An AWS Python Pulumi program"""
 
+import os
+
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
+import pulumi_std as std
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+root = pulumi.get_root_directory()
+project_root = os.path.abspath(os.path.dirname(root))
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+
+bucket = aws.s3.Bucket(
+    "test-bucket",
+    tags={
+        "Project": "GDPR Obfuscator",
+        "Environment": "Dev",
+    },
+)
+
+complex_pii_data_path = os.path.join(project_root, "tests/data/complex_pii_data.csv")
+
+test_data_complex_pii_data = aws.s3.BucketObject(
+    "complex_pii_data",
+    bucket=bucket.id,
+    key="complex_pii_data.csv",
+    source=pulumi.FileAsset(complex_pii_data_path),
+    etag=std.filemd5(input=complex_pii_data_path).result,
+)
+
+
+pulumi.export("bucket_name", bucket.id)
+pulumi.export("complex_pii_data_key", test_data_complex_pii_data.key)
