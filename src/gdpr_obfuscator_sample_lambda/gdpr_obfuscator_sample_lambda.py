1d285ed8124016ab18d88bd313ad1194a0a16ee3
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-09-29 20:33:42 -0300

    🔧 chore(lambda): add logging and error handling to gdpr obfuscator sample lambda

--- src/gdpr_obfuscator_sample_lambda/gdpr_obfuscator_sample_lambda.py
+++ src/gdpr_obfuscator_sample_lambda/gdpr_obfuscator_sample_lambda.py
@@ -1,30 +1,52 @@
+import logging
 from pathlib import Path
 
 import boto3
 
 from gdpr_obfuscator import gdpr_obfuscator
 
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, context):
-    s3_client = boto3.client("s3")
+    logger.info("Starting lambda_handler", extra={"event": event})
 
-    filename_base = Path(event["file_to_obfuscate"]).stem
-    file_extension = Path(event["file_to_obfuscate"]).suffix
+    try:
+        s3_client = boto3.client("s3")
 
-    result = gdpr_obfuscator(event["file_to_obfuscate"], event["pii_fields"])
+        filename_base = Path(event["file_to_obfuscate"]).stem
+        file_extension = Path(event["file_to_obfuscate"]).suffix
 
-    result_filename = f"{filename_base}_obfuscated{file_extension}"
+        result = gdpr_obfuscator(event["file_to_obfuscate"], event["pii_fields"])
 
-    result_s3_address = f"s3://{event['destination_bucket']}/{result_filename}"
+        logger.info("Obfuscation complete", extra={"result": result})
 
-    s3_client.put_object(
-        Bucket=event["destination_bucket"],
-        Key=result_filename,
-        Body=result,
-        ContentType="text/csv",
-    )
+        result_filename = f"{filename_base}_obfuscated{file_extension}"
 
-    return {
-        "statusCode": 200,
-        "body": result_s3_address,
-    }
+        result_s3_address = f"s3://{event['destination_bucket']}/{result_filename}"
+
+        response = s3_client.put_object(
+            Bucket=event["destination_bucket"],
+            Key=result_filename,
+            Body=result,
+            ContentType="text/csv",
+        )
+
+        logger.info("File uploaded to S3", extra={"s3_address": result_s3_address})
+
+        result = {
+            "statusCode": 200,
+            "body": result_s3_address,
+        }
+
+        return result
+
+    except Exception as e:
+        logger.error(e)
+
+        return {
+            "statusCode": 500,
+            "body": str(e),
+        }
