1501b9ca4ad9b22bb1134e26b9871c2a28f1b940
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-10-01 00:17:02 -0300

    🏗️ build(lambda): split lambda layer into separate dependency and application layers

--- infrastructure/components/lambda_function.py
+++ infrastructure/components/lambda_function.py
@@ -4,6 +4,9 @@ import os
 
 import pulumi
 import pulumi_aws as aws
+from helpers.build_lambda_layer import (
+    build_lambda_dependency_requirements,
+)
 
 
 def create_lambda_function(
@@ -16,17 +19,32 @@ def create_lambda_function(
     """Create Lambda function for GDPR obfuscation processing"""
 
     lambda_function_file_path = os.path.join(project_root, f"src/{lambda_name}")
-    lambda_layer_file_path = os.path.join(
-        project_root, "dist/gdpr-obfuscator-layer.zip"
+    lambda_function_archive = pulumi.FileArchive(lambda_function_file_path)
+
+    build_lambda_dependency_requirements(
+        project_root,
+        "build/layers/deps/python",
+        "build/layers/gdpr_obfuscator/python",
     )
 
-    lambda_function_archive = pulumi.FileArchive(lambda_function_file_path)
-    lambda_layer_archive = pulumi.FileArchive(lambda_layer_file_path)
-
-    lambda_layer = aws.lambda_.LayerVersion(
-        f"{lambda_name}-layer",
-        layer_name=f"{lambda_name}-layer",
-        code=lambda_layer_archive,
+    lambda_deps_layer_file_path = os.path.join(project_root, "build/layers/deps")
+    lambda_deps_layer_archive = pulumi.FileArchive(lambda_deps_layer_file_path)
+    lambda_deps_layer = aws.lambda_.LayerVersion(
+        f"{lambda_name}-deps-layer",
+        layer_name=f"{lambda_name}-deps-layer",
+        code=lambda_deps_layer_archive,
+        compatible_runtimes=["python3.13"],
+    )
+    lambda_gdpr_obfuscator_layer_file_path = os.path.join(
+        project_root, "build/layers/gdpr_obfuscator"
+    )
+    lambda_gdpr_obfuscator_layer_archive = pulumi.FileArchive(
+        lambda_gdpr_obfuscator_layer_file_path
+    )
+    lambda_gdpr_obfuscator_layer = aws.lambda_.LayerVersion(
+        f"{lambda_name}-gdpr-obfuscator-layer",
+        layer_name=f"{lambda_name}-gdpr-obfuscator-layer",
+        code=lambda_gdpr_obfuscator_layer_archive,
         compatible_runtimes=["python3.13"],
     )
 
@@ -37,7 +55,7 @@ def create_lambda_function(
         handler=f"{lambda_name}.lambda_handler",
         code=lambda_function_archive,
         timeout=200,
-        layers=[lambda_layer.arn],
+        layers=[lambda_deps_layer.arn, lambda_gdpr_obfuscator_layer.arn],
         logging_config={
             "log_format": "JSON",
             "application_log_level": "INFO",
